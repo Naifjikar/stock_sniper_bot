96f5927365b8426f54c577bd6dcf21789032d630
Naifjikar <131428263+Naifjikar@users.noreply.github.com>
2025-07-11 19:11:04 +0300

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import asyncio
 from telegram import Bot
 
-# التوكن ومعرف القناة
 TOKEN = '8085180830:AAGHgsKIdVSFNCQ8acDiL8gaulduXauN2xk'
 CHANNEL_ID = '-1002608482349'
 
-# إنشاء البوت
 bot = Bot(token=TOKEN)
 
-# إرسال رسالة واحدة فقط
-bot.send_message(chat_id=CHANNEL_ID, text="🚀 بداية انطلاق: $TEST")
+async def main():
+    await bot.send_message(chat_id=CHANNEL_ID, text="🚀 بداية انطلاق: $TEST")
+
+# تشغيل الكود
+asyncio.run(main())
